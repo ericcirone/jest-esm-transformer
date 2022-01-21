f9feaec7e9e295a4cb67e1b8d14502e09972414e
Eric Cirone <ecirone@gmail.com>
2022-01-21 14:10:57 -0500

    updates to hopefully allow code splitting

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const babel = require("@babel/core");
 
 module.exports = {
-  process(src) {
-    const options = {
-      babelrc: false,
-      compact: false,
-      plugins: [require.resolve("@babel/plugin-transform-modules-commonjs")]
-    };
+	process(src) {
+		const options = {
+			babelrc: false,
+			compact: false,
+			plugins: [require.resolve("@babel/plugin-transform-modules-commonjs"), require.resolve("@babel/dynamic-import-node")],
+		};
 
-    return babel.transform(src, options).code;
-  }
+		return babel.transform(src, options).code;
+	},
 };
